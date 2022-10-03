5e047c854159b135f8f65227cc33e65790bcb62e
Peter Hagenson <p.hagenson@gmail.com>
2022-10-03 13:31:35 -0500

    addPatient POST route done, NEED req.user.id to complete

--- server/routes/patient.router.js
+++ server/routes/patient.router.js
@@ -13,9 +13,17 @@ router.get('/', (req, res) => {
  * POST route template
  */
 router.post('/', (req, res) => {
+  //TODO - need to get req.user.id!!!!
   console.log("in patient router", req.body, req.user);
-
-  // POST route code here
+  const query = `INSERT INTO "patient" ("first_name", "last_name", "email", "id", "clinician_id")
+  VALUES ($1, $2, $3, $4);`;
+  pool.query(query, [req.body.firstName, req.body.lastName, req.body.email, req.body.patientId, req.user.id])
+    .then(response => {
+      res.sendStatus(200);
+    }).catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    })
 });
 
 module.exports = router;
