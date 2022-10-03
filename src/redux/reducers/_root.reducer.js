dc1403315bcbbe37983af513f630ab3b5b26bbd5
AJ Timek <Timek612@gmail.com>
2022-10-03 16:15:35 -0500

    working on user registration. data is being sent to router but not complete yet.

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
-import institution from './institution.reducer';
+// import institution from './institution.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -12,7 +12,7 @@ import institution from './institution.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  institution,
+  // institution,
 });
 
 export default rootReducer;
