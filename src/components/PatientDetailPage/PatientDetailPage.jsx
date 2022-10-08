7612144cb5fa685b36caab9b24f5f91aeffac04d
Peter Hagenson <p.hagenson@gmail.com>
2022-10-08 10:48:11 -0500

    layoud patientDetailDone

--- src/components/PatientDetailPage/PatientDetailPage.jsx
+++ src/components/PatientDetailPage/PatientDetailPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import csvDownload from 'json-to-csv-export'
+import './PatientDetail.css';
 
 
 
@@ -12,21 +13,14 @@ function PatientDetail() {
 
 
   // contains array of patients displayed in dropdown menu
-  // const patients = useSelector((store) => store.patients);
-  // // contains data for individual patient selected in dropdown menu
-  // const patientData = useSelector((store) => store.patientData)
-  // //contains the id of the patient selected in the dropdown menu
-  // const [patientId, setPatientId] = useState(' ');
-
-  // this function dispatches the id of the patient selected in the dropdown menu to the getPatientData() function in the patient.saga file
-
-
   const patients = useSelector((store) => store.patients);
+  // contains data for individual patient selected in dropdown menu
   const patientData = useSelector((store) => store.patientData.patientData)
   const jsonData = useSelector((store) => store.patientData.processedData)
   console.log(jsonData);
   console.log(patientData);
 
+  // //contains the id of the patient selected in the dropdown menu
   const [patientId, setPatientId] = useState(' ');
 
   const dataToConvert = {
@@ -36,6 +30,7 @@ function PatientDetail() {
     headers: ['ID', 'Session ID', '% time on drugs', '% time on controlled', '% time on neither', '% time on drugs no back', '% time non drugs no back']
   }
 
+  // this function dispatches the id of the patient selected in the dropdown menu to the getPatientData() function in the patient.saga file
   const getPatientData = () => {
     event.preventDefault();
     // console.log("getPatientData", patientId);
@@ -81,38 +76,42 @@ function PatientDetail() {
 
   return (
     <div>
-      <form onSubmit={getPatientData}>
-        <select onChange={(event) => setPatientId(event.target.value)} name="patient" id="patientSelect">
-          <option value="initial">Select A Patient</option>
+      <div className="btnRowDiv">
+        <div className="selectMenu">
+          <select onChange={(event) => setPatientId(event.target.value)} name="patient" id="patientSelect">
+            <option value="initial">Select A Patient</option>
 
-          {patients && patients.map(patient => {// loops over all the institutions and displays them as options
-            if (patient.is_active === true) {
-              return (
-                <option key={patient.id} value={patient.id}>{patient.first_name} {patient.last_name}</option>
-              )
-            }
-          })}
+            {patients && patients.map(patient => {// loops over all the institutions and displays them as options
+              if (patient.is_active === true) {
+                return (
+                  <option key={patient.id} value={patient.id}>{patient.first_name} {patient.last_name}</option>
+                )
+              }
+            })}
+          </select>
+          <button className="getDataBtn" onClick={getPatientData}>Get Data</button>
+        </div>
 
-        </select>
-        <button type="submit">Get Data</button>
-      </form>
+        <div className="patientDetailBtns">
+          <button onClick={toAddPatientForm}>New Patient</button>
+          <button className="deletePatientBtn" onClick={deletePatient}>Delete Patient</button>
+        </div>
+      </div>
+      <br />
+      <div className="exportBtnDiv">
+        <button onClick={() => exportJsonData()}>Export Data</button>
+      </div>
 
+      {/* <div>
+        {patientData && patientData.is_active === true && JSON.stringify(patientData)} */}
 
-
-      <button onClick={toAddPatientForm}>New Patient</button>
-      <button onClick={deletePatient}>Delete Patient</button>
-      <button onClick={() => exportJsonData()}>Export</button>
-
+      {/* {JSON.stringify(patients)} */}
       <div>
+        {/* {conditionalData} */}
         {patientData && patientData.is_active === true && JSON.stringify(patientData)}
 
-        {/* {JSON.stringify(patients)} */}
-        <div>
-          {/* {conditionalData} */}
-          {patientData && patientData && patientData.is_active === true && JSON.stringify(patientData)}
-
-        </div>
-      </div >
+      </div>
+      {/* </div > */}
     </div>
   );
 
