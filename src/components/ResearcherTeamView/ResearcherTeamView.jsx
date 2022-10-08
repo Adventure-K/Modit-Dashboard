0f9e174e21a8477387f35a6499ed740a9abf8e36
Peter Hagenson <p.hagenson@gmail.com>
2022-10-08 15:12:50 -0500

    basic formatting done

--- src/components/ResearcherTeamView/ResearcherTeamView.jsx
+++ src/components/ResearcherTeamView/ResearcherTeamView.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useHistory, useParams } from 'react-router-dom'
+import './ResearcherTeamView.css';
 
 function PatientDetail() {
   const dispatch = useDispatch()
@@ -49,31 +50,37 @@ function PatientDetail() {
   return (
     <div>
       {/* <h2>{heading}</h2> */}
-      <form onSubmit={getPatientData}>
-        <select
-          onChange={(event) => setPatientId(event.target.value)}
-          name="patient"
-          id="patientSelect"
-        >
-          <option value="initial">Select A Patient</option>
+      <div className="btnRowDiv">
+        <div className="selectMenu">
+          <select
+            onChange={(event) => setPatientId(event.target.value)}
+            name="patient"
+            id="patientSelect"
+          >
+            <option value="initial">Select A Patient</option>
 
-          {patients.map((patient) => {
-            // loops over all the institutions and displays them as options
-            if (patient.is_active === true) {
-              return (
-                <option key={patient.id} value={patient.id}>
-                  {patient.first_name} {patient.last_name}
-                </option>
-              )
-            }
-          })}
-        </select>
-        <button type="submit">Get Data</button>
-      </form>
+            {patients.map((patient) => {
+              // loops over all the institutions and displays them as options
+              if (patient.is_active === true) {
+                return (
+                  <option key={patient.id} value={patient.id}>
+                    {patient.first_name} {patient.last_name}
+                  </option>
+                )
+              }
+            })}
+          </select>
+          <button className="getDataBtn" onClick={getPatientData}>Get Data</button>
+        </div>
 
-      <button onClick={toAddPatientForm}>New Patient</button>
-      <button onClick={deletePatient}>Delete Patient</button>
-      <button>Export</button>
+        <div className="patientDetailBtns">
+          <button className="patientDetailBtns" onClick={toAddPatientForm}>New Patient</button>
+          <button className="deletePatientBtn" onClick={deletePatient}>Delete Patient</button>
+        </div>
+      </div>
+      <div className="exportBtnDiv">
+        <button>Export</button>
+      </div>
       {/* {JSON.stringify(patients)} */}
       <div>
         {/* {conditionalData} */}
