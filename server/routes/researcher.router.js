19f1ff3345fa5a421505b76694880afe7e0c591a
Peter Hagenson <p.hagenson@gmail.com>
2022-10-08 19:36:18 -0500

    researcherViewLayout + fixed GET clinicians route

--- server/routes/researcher.router.js
+++ server/routes/researcher.router.js
@@ -4,23 +4,28 @@ const router = express.Router();
 
 // this GET route is to get all clinicians associated with a researcher and institution
 router.get('/clinicians', (req, res) => {
-    const query = `
-    SELECT * FROM "user"
-    WHERE "researcher_id" = $1
-    ORDER BY "first_name" ASC;`;
-    pool.query(query, [req.user.id])
-      .then(result => {
-        res.send(result.rows);
-      })
-      .catch(err => {
-        console.log('Error in getting clinician list for researcher', err);
-        res.sendStatus(500)
-      })
-  });
+  console.log('in get clinicians', req.user)
+  // const query = `
+  //   SELECT * FROM "user"
+  //   WHERE "researcher_id" = $1
+  //   ORDER BY "first_name" ASC;`;
+  const query = `SELECT * FROM "user"
+  WHERE inst_id = $1 AND user_level = 0
+  ORDER BY first_name ASC;`;
+  // pool.query(query, [req.user.id])
+  pool.query(query, [req.user.inst_id])
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('Error in getting clinician list for researcher', err);
+      res.sendStatus(500)
+    })
+});
 
-  // this GET route is to get all clinicians associated with a researcher and institution
+// this GET route is to get all clinicians associated with a researcher and institution
 router.get('/teamData', (req, res) => {
-    const query = `
+  const query = `
     SELECT "patient".username, "patient".clinician_id, "patient".first_name, "patient".last_name, "session_data".*, "session".* FROM "session_data"
     JOIN "session"
     ON "session_data".session_id = "session".id
@@ -29,19 +34,19 @@ router.get('/teamData', (req, res) => {
     JOIN "user"
     ON "patient".clinician_id = "user".id
     WHERE "researcher_id" = $1;`;
-    pool.query(query, [req.user.id])
-      .then(result => {
-        res.send(result.rows);
-      })
-      .catch(err => {
-        console.log('Error in getting team data for researcher', err);
-        res.sendStatus(500)
-      })
-  });
+  pool.query(query, [req.user.id])
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('Error in getting team data for researcher', err);
+      res.sendStatus(500)
+    })
+});
 
 //GET route for researcher team clinicians
 router.get('/researcherTeam/:id', (req, res) => {
-  console.log("in get router")
+  // console.log("in get router")
   const query = `SELECT * FROM "patient" WHERE clinician_id = $1;`;
 
   pool.query(query, [req.params.id])
@@ -56,20 +61,20 @@ router.get('/researcherTeam/:id', (req, res) => {
 
 // to get the institution related to the logged in researcher
 router.get('/researchInst', (req, res) => {
-    console.log('the user who is logged in is', req.user.id);
-    const query = `SELECT "institution".name FROM "institution"
+  // console.log('the user who is logged in is', req.user.id);
+  const query = `SELECT "institution".name FROM "institution"
     JOIN "user"
     ON "institution".id = "user".inst_id
     WHERE "user".id = $1;`;
-    pool.query(query,[req.user.id])
-      .then(result => {
-        res.send(result.rows);
-      })
-      .catch(err => {
-        console.log('Error in getting institution for researcher', err);
-        res.sendStatus(500)
-      })
-  });
+  pool.query(query, [req.user.id])
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('Error in getting institution for researcher', err);
+      res.sendStatus(500)
+    })
+});
 
 
 
