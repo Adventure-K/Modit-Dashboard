01b44a611513521558abf4cafa1287994aaa34d6
Peter Hagenson <p.hagenson@gmail.com>
2022-10-10 13:21:20 -0500

    moditAPI complete

--- server/routes/modit.router.js
+++ server/routes/modit.router.js
@@ -4,9 +4,16 @@ const router = express.Router();
 
 
 router.post('/', (req, res) => {
+    console.log("in modit router post", req.body)
     const query = `INSERT INTO "session" ("modit_id", "proportionOfGazeTimeOnDrugs", "proportionOfGazeTimeOnNonDrugs", "proportionOfGazeTimeOnBack", "proportionOfGazeTimeOnDrugsNoBack", "proportionOfGazeTimeOnNonDrugsNoBack")
-    VALUES = $1, $2, $3, $4, $5, $6;`;
+    VALUES  ($1, $2, $3, $4, $5, $6);`;
     pool.query(query, [req.body.modit_id, req.body.proportionOfGazeTimeOnDrugs, req.body.proportionOfGazeTimeOnNonDrugs, req.body.proportionOfGazeTimeOnBack, req.body.proportionOfGazeTimeOnDrugsNoBack, req.body.proportionOfGazeTimeOnNonDrugsNoBack])
+        .then(response => {
+            res.sendStatus(200);
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        })
 });
 
 module.exports = router;
