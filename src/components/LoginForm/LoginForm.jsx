21b2dc61587f5c2609acba4baf222f78b20389f8
Josh Snyder <joshua.t.snyder@gmail.com>
2022-10-12 10:52:47 -0500

    figuring out some tailwind

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -44,6 +44,7 @@ function LoginForm() {
             required
             value={username}
             onChange={(event) => setUsername(event.target.value)}
+            className="w-full border border-gray-900 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-cyan-500 focus:ring-1 focus:ring-cyan-500"
           />
         </label>
       </div>
@@ -56,11 +57,12 @@ function LoginForm() {
             required
             value={password}
             onChange={(event) => setPassword(event.target.value)}
+            className="w-full border border-gray-900 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-cyan-500 focus:ring-1 focus:ring-cyan-500"
           />
         </label>
       </div>
       <div>
-        <input className="btn" type="submit" name="submit" value="Log In" />
+        <input className="inline-block rounded-lg bg-blue-600 text-white leading-normal uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-20 h-9" type="submit" name="submit" value="Log In" />
       </div>
     </form>
   );
