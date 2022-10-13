2e7eb5c801a8ba04f6871e6c6ea52aa02b47151a
Peter Hagenson <p.hagenson@gmail.com>
2022-10-13 15:11:59 -0500

    changes made

--- src/components/InstitutionManageAccountsPageAdmin/InstitutionManageAccountsPageAdmin.jsx
+++ src/components/InstitutionManageAccountsPageAdmin/InstitutionManageAccountsPageAdmin.jsx
@@ -97,13 +97,13 @@ function InstitutionManageAccountsPageAdmin() {
                   <p><span><button onClick={() => (deleteRequest(user.id))}>Delete</button></span><span><button onClick={() => (approveRequest(user.id))}>Approve</button></span>{user.first_name} {user.last_name}</p>
                 </div>
               )
-            } else if (user.is_approved === false && loggedInUser.user_level == 3) {
-              return (
-                <div>
-                  <p>{user.first_name} {user.last_name}</p>
-                </div>
+              // } else if (user.is_approved === false && loggedInUser.user_level == 3) {
+              //   return (
+              //     <div>
+              //       <p>{user.first_name} {user.last_name}</p>
+              //     </div>
 
-              )
+              //   )
             }
           })}
 
