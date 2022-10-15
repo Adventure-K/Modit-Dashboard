9ed2b77caead45599f6bd15ceb1300e9b61a124a
AJ Timek <Timek612@gmail.com>
2022-10-15 13:21:04 -0500

    cleared data reducers on load of page "userDetails"

--- src/components/ResearcherTeamView/ResearcherTeamView.jsx
+++ src/components/ResearcherTeamView/ResearcherTeamView.jsx
@@ -29,6 +29,7 @@ function ResearcherTeamView() {
   const [patientId, setPatientId] = useState(' ')
 
   useEffect(() => {
+    
     dispatch({
       type: 'FETCH_TEAM_PATIENTS',
       payload: params.id,
